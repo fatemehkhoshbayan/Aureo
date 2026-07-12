524fb70c9966731b7531f3acdb9808ddccb9b5d2
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2026-07-12 12:21:52 -0400

    temp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { MainLayout } from '@/layout';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [],
+  },
+];
