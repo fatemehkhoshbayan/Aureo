8be39659839cbcfdc7046e199d1b71ccb39b2c74
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2026-07-12 19:49:33 -0400

    feat: implement header component with navigation links and branding; add constants for navigation items

---
+++ src/app/layout/header/constants.ts
@@ -0,0 +1,20 @@
+export const NAV_LINKS = [
+  {
+    id: 'services',
+    label: 'Services',
+    href: '/',
+    icon: 'icon-[lucide--camera]',
+  },
+  {
+    id: 'my-bookings',
+    label: 'My Bookings',
+    href: '/my-bookings',
+    icon: 'icon-[lucide--calendar]',
+  },
+  {
+    id: 'my-profile',
+    label: 'My Profile',
+    href: '/my-profile',
+    icon: 'icon-[lucide--user]',
+  },
+];
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NAV_LINKS } from './constants';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styles: ``,
 })
-export class Header {}
+export class Header {
+  protected readonly NAV_LINKS = NAV_LINKS;
+}
