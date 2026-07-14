d1972f4b0f3c4ba660744543bf7f9bfab76773c6
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2026-07-14 17:22:40 -0400

    feat: add SkeletonCard component for loading placeholders in UI

---
+++ src/app/shared/skeleton-card/skeleton-card.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-card',
+  imports: [],
+  standalone: true,
+  templateUrl: './skeleton-card.html',
+  styles: ``,
+})
+export class SkeletonCard {}
